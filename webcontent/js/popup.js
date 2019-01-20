dd43bef0af8dbf2f45222f1fc1b1bf07afc6eee6
royel21 <royelconsoro@hotmail.com>
2019-01-19 23:17:45 -0400

    some fix

--- webcontent/js/popup.js
+++ webcontent/js/popup.js
@@ -1,18 +1,22 @@
 var $popup = $('#popup')
-
+var inEl;
 popupHide = (e) => {
     $popup.css({
         display: "none"
     }).text("");
     $popup.removeClass('popup-top');
+    inEl = false;
 }
 
+
 popupShow = async (event) => {
+    inEl = true;
+
     var element = $(event.target).hasClass('popup-msg') ?
         event.target : $(event.target).closest(".popup-msg")[0];
 
     if (element != undefined) {
-
+        
         var rect = element.getBoundingClientRect();
         var msg = element.dataset.title;
         var li = $(element).closest('li')[0];
@@ -21,31 +25,36 @@ popupShow = async (event) => {
                 where: {
                     Id: li.id.replace('file-', '')
                 },
-                include: { model: db.Folder }
+                include: {
+                    model: db.Folder
+                }
             });
-            if (file) {
+            if (file){
                 msg = path.join(file.folder.Name, file.Name);
             }
         }
-        $popup.css({
-            display: "block",
-            top: -3000,
-        }).text(msg == undefined ? element.textContent : msg);
 
-        var top = rect.top + 8 + rect.height;
-        if (top + $popup.height() + 10 > window.innerHeight) {
-            top = rect.top - 22 - $popup.height()
-            $popup.addClass('popup-top');
+        if (inEl) {
+            $popup.css({
+                display: "block",
+                top: -3000,
+            }).text(msg == undefined ? element.textContent : msg);
+
+            var top = rect.top + 8 + rect.height;
+            if (top + $popup.height() + 10 > window.innerHeight) {
+                top = rect.top - 22 - $popup.height()
+                $popup.addClass('popup-top');
+            }
+
+            $popup.css({
+                top,
+                left: (rect.x + rect.width / 2) - ($popup.width() / 2) - 9
+            });
         }
-
-        $popup.css({
-            top,
-            left: (rect.x + rect.width / 2) - ($popup.width() / 2) - 9
-        });
     }
 }
 
 $('body, .footer').on('mouseenter', '.popup-msg', popupShow);
 $('body, .footer').on('mouseleave wheel', '.popup-msg', popupHide);
 $('body, .footer').on('click', popupHide);
-$('.cancel-footer').on('mouseenter',popupHide);
+$('.cancel-footer').on('mouseenter', popupHide);
